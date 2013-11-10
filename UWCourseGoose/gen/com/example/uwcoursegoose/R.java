a696bd669ffecb161b693c95fd7b2010edce5e73
WannabeSmith <ianwaudby-smith@live.ca>
2013-11-10 00:22:39 -0500

    12:22

--- UWCourseGoose/gen/com/example/uwcoursegoose/R.java
+++ UWCourseGoose/gen/com/example/uwcoursegoose/R.java
@@ -11,6 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int AHS=0x7f070001;
+        public static final int Arts=0x7f070002;
+        public static final int Engineering=0x7f070003;
+        public static final int Environment=0x7f070004;
+        public static final int Mathematics=0x7f070005;
+        public static final int Science=0x7f070006;
         public static final int white=0x7f070000;
     }
     public static final class dimen {
@@ -24,8 +30,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash_background=0x7f020001;
+        public static final int gradient=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int splash_background=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f090001;
