ee8810df11ee985edcd3aebb9dfcfcc1b7adcb23
RaunaqSuri <raunaq.suri@hotmail.com>
2014-03-02 22:47:10 -0500

    created data folder

---
+++ data/getFaculties.py
@@ -0,0 +1,10 @@
+#@Author Raunaq Suri
+#@Date December 31, 2013
+#Retrieves all the faculties of Waterloo
+
+import json
+import urllib.request
+import urllib.parse
+from pprint import pprint
+
+print("hello world")
