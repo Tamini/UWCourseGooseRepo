4323a1f8afc30409bc12a2ed3be797ce38109f3b
RaunaqSuri <raunaq.suri@hotmail.com>
2014-03-02 23:16:02 -0500

    added all the faculties. The Api key must be inputted in order for it to work!

--- data/getFaculties.py
+++ data/getFaculties.py
@@ -7,4 +7,32 @@ import urllib.request
 import urllib.parse
 from pprint import pprint
 
-print("hello world")
+print("Getting all the faculties of UW")
+
+
+baseUrl = "https://api.uwaterloo.ca/v2/codes/groups.json"
+api_key = input("Enter the api key here: ") #asked to enter api key because having the api key as open source in code is not a smart idea
+
+url = baseUrl+"?key="+api_key
+
+website = urllib.request.urlopen(url)
+
+#gets the user's data
+data = json.loads(website.read().decode("utf-8"))
+
+#creates the file to write to
+
+faculties = open('faculties.csv', 'w+')
+
+for items in data['data']:
+
+	#gets the names and the faculty codes
+	code = (items['group_code'])
+	name = (items['group_short_name'])
+
+	lineToWrite = code+","+name
+
+	faculties.write(lineToWrite + "\n")
+
+faculties.close()
+input("Enter any key to exit: ")
